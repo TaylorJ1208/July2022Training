0a0ff880d744f5ec157571661af26c48e48196ab
Taylor Joostema <taylor.joostema@hcl.com>
2022-08-02 13:50:05 -0400

    Updated 08-02-2022

--- assignments/August/08-02-2022/src/main/java/com/taylor/model/Employee.java
+++ assignments/August/08-02-2022/src/main/java/com/taylor/model/Employee.java
@@ -7,6 +7,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 	private int id;
 	private String empFirstName;
@@ -17,25 +24,6 @@ public class Employee {
 	private String empContact;
 	private boolean isAdmin;
 	private SQLConnection conn;
-	
-	
-	public Employee() throws SQLException, ClassNotFoundException {
-		super();
-	}
-	
-	public Employee(int id, String empFirstName, String empLastName,
-			String empUsername, String empPassword, String empAddress,
-			String empContact, boolean isAdmin) throws SQLException, ClassNotFoundException {
-		super();
-		this.id = id;
-		this.empFirstName = empFirstName;
-		this.empLastName = empLastName;
-		this.empUsername = empUsername;
-		this.empPassword = empPassword;
-		this.empAddress = empAddress;
-		this.empContact = empContact;
-		this.isAdmin = isAdmin;
-	}
 	
 	public boolean validateUserNamePwd() throws SQLException, ClassNotFoundException {		
 		PreparedStatement statement = conn.getConnection().prepareStatement("SELECT * FROM employee WHERE username=? AND password=?;");
@@ -94,73 +82,7 @@ public class Employee {
 		ResultSet result = statement.executeQuery("SELECT * FROM employee");
 		return result;
 		}
-
-	public int getId() {
-		return id;
 	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getEmpFirstName() {
-		return empFirstName;
-	}
-
-	public void setEmpFirstName(String empFirstName) {
-		this.empFirstName = empFirstName;
-	}
-
-	public String getEmpLastName() {
-		return empLastName;
-	}
-
-	public void setEmpLastName(String empLastName) {
-		this.empLastName = empLastName;
-	}
-
-	public String getEmpUsername() {
-		return empUsername;
-	}
-
-	public void setEmpUsername(String empUsername) {
-		this.empUsername = empUsername;
-	}
-
-	public String getEmpPassword() {
-		return empPassword;
-	}
-
-	public void setEmpPassword(String empPassword) {
-		this.empPassword = empPassword;
-	}
-
-	public String getEmpAddress() {
-		return empAddress;
-	}
-
-	public void setEmpAddress(String empAddress) {
-		this.empAddress = empAddress;
-	}
-
-	public String getEmpContact() {
-		return empContact;
-	}
-
-	public void setEmpContact(String empContact) {
-		this.empContact = empContact;
-	}
-
-	public boolean isAdmin() {
-		return isAdmin;
-	}
-
-	public void setAdmin(boolean isAdmin) {
-		this.isAdmin = isAdmin;
-	}
-	
-	}
-
 
 class SQLConnection {
 	private static Connection conn = null;
